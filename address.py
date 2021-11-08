ff004bb9acfffc15dfa1136170a572cf3b280588
hitesh <hitesh22rana@gmail.com>
2021-11-09 00:09:27 +0530

    ip and mac address finder for windows

---
+++ address.py
@@ -0,0 +1,53 @@
+# Modules
+import os
+from colorama import Fore
+
+def ip_and_mac_address():
+    raw_address = os.popen('ipconfig /all').read()
+    address_all = raw_address.split('\n')
+    
+    ip_address = []
+    mac_address = []
+    host_names = []
+    
+    for individual_name in address_all:
+        if('Host Name . . . . . . . . . . . . :') in individual_name:
+            host_names.append(individual_name.replace('Host Name . . . . . . . . . . . . :', '').strip())     
+
+        if('IPv4 Address. . . . . . . . . . . :') in individual_name:
+            ip_address.append(individual_name.replace('IPv4 Address. . . . . . . . . . . :', '').strip())
+        
+        if('Physical Address. . . . . . . . . :') in individual_name:
+            mac_address.append(individual_name.replace('Physical Address. . . . . . . . . :', '').strip())
+
+    # Host name
+    if(len(host_names) == 0):
+        print(Fore.RED + "\nHost Name is not found\n")
+    
+    else:
+        print(Fore.RED + "\nHost Name : " , end = ' ')
+        for individual_host in host_names:
+            print(Fore.GREEN + individual_host , end = '  ')
+
+    # IP Address's
+    if(len(ip_address) == 0):
+        print(Fore.RED + "No information of any IP address\n")
+
+    else:
+        print(Fore.RED + "\nAll gathered IP Address's : " , end = ' ')
+        for individual_ip in ip_address:
+            print(Fore.GREEN + individual_ip , end = '  ')
+   
+    # MAC/Physical Address's
+    if(len(mac_address) == 0):
+        print(Fore.RED + "No information of any MAC/Physical address\n")
+    
+    else:
+        print(Fore.RED + "\nAll gathered Physical/MAC Address's : " , end = ' ')
+        for individual_mac in mac_address:
+            print(Fore.GREEN + individual_mac , end = '  ')
+
+    print('\n')
+    
+if __name__ == '__main__':
+    ip_and_mac_address()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# modules
+# Modules
 from logo import logo
 from email_scraper import email_scraper
 from password_grabber import passwords
+from address import ip_and_mac_address
 from colorama import Fore
 from termcolor import colored
 import logging, coloredlogs
@@ -16,6 +17,7 @@ logo()
 print(Fore.RED+'''
 [1] Scrape all Email's from target website
 [2] Get all Wifi password's 
+[3] Get IP and MAC address
 ''')
 
 # Menu-input
@@ -27,3 +29,6 @@ if(menu_input == 1):
 
 elif(menu_input == 2):
     passwords()
+
+elif(menu_input == 3):
+    ip_and_mac_address()
